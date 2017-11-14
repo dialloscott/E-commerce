005865a6dc89ccfb3b2cace37b3b027736b8acba
= <=>
2017-11-14 14:32:50 +0100

    'XXX'

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ $url = parse_url(getenv("DATABASE_URL"));
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$database = substr($url["path"], 1);
+$database = ltrim($url["path"],'/');
 
 return [
 
